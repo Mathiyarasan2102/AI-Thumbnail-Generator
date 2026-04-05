3684f394304b9fa5f86cf9c12de4c8a35516c46e
Mathi <mathiarasan.2102@gmail.com>
2026-04-05 23:05:12 +0530

    Enhance Generate page with ColorSchemeSelector and PreviewPanel components; update routing and StyleSelector functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/generate" element={<Generate />} />
-                <Route path="/generate/id" element={<Generate />} />
+                <Route path="/generate/:id" element={<Generate />} />
                 <Route path="/my-generation" element={<MyGeneration />} />
                 <Route path="/preview" element={<YtPreview />} />
             </Routes>
--- client/src/pages/Generate.jsx
+++ client/src/pages/Generate.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import SoftBackdrop from "../components/SoftBackdrop";
 import AspectRatioSelector from "../components/AspectRatioSelector";
-import { colorSchemes } from "../assets/assets";
+import { colorSchemes, dummyThumbnails } from "../assets/assets";
 import StyleSelector from '../components/StyleSelector'
+import ColorSchemeSelector from '../components/ColorSchemeSelector'
+import PreviewPanel from "../components/PreviewPanel";
 
 const Generate = () => {
   const { id } = useParams();
@@ -16,12 +18,35 @@ const Generate = () => {
   const [style, setStyle] = useState('Bold & Graphic');
   const [styleDropdownOpen, setStyleDropdownOpen] = useState(false);
 
+  const handleGenerate = async () => {
+
+  }
+
+  const fetchThumbnail = async () => {
+    if(id){
+      const thumbnail = dummyThumbnails.find((thumbnail)=>thumbnail._id === id);
+      setThumbnail(thumbnail)
+      setAdditionalDetails(thumbnail.user_prompt)
+      setTitle(thumbnail.title)
+      setColorSchemeId(thumbnail.color_scheme)
+      setAspectRatio(thumbnail.aspect_ratio)
+      setStyle(thumbnail.style)
+      setLoading(false)
+    }
+  }
+
+  useEffect(()=>{
+    if(id){
+      fetchThumbnail()
+    }
+  },[id])
+
 
   return (
     <>
       <SoftBackdrop />
       <div className="pt-24 min-h-screen">
-        ,<main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-28 lg:pb-8">
+        <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-28 lg:pb-8">
           <div className="grid lg:grid-cols-[400px_1fr] gap-8">
             {/* Left Column */}
             <div className={`space-y-6 ${id && 'pointer-events-none'}`}>
@@ -44,8 +69,9 @@ const Generate = () => {
                   {/* AspectRatioSelector */}
                   <AspectRatioSelector value={aspectRatio} onChange={setAspectRatio} />
                   {/* StyleSelector */}
-                  <StyleSelector value={style} onChange = {setStyle} isOpen={styleDropdownOpen} setIsOpen={setStyleDropdownOpen}/>
+                  <StyleSelector value={style} onChange={setStyle} isOpen={styleDropdownOpen} setIsOpen={setStyleDropdownOpen} />
                   {/* ColorSchemeSelector */}
+                  <ColorSchemeSelector value={colorSchemeId} onChange={setColorSchemeId} />
 
                   {/* Details */}
                   <div className="space-y-2">
@@ -56,9 +82,10 @@ const Generate = () => {
                   </div>
 
                 </div>
+
                 {/*Button */}
                 {!id && (
-                  <button className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600 hover:from-pink-700 disabled:cursor-not-allowed transition-colors">
+                  <button onClick={handleGenerate} className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600 hover:from-pink-700 disabled:cursor-not-allowed transition-colors">
                     {loading ? 'Generating...' : 'Generate Thumbnail'}
                   </button>
                 )
@@ -68,7 +95,10 @@ const Generate = () => {
             </div>
             {/* Right Column */}
             <div>
-
+              <div className="p-6 rounded-2xl bg-white/8 border border-white/10 shadow-xl">
+                <h2 className="text-lg font-semibold text-zinc-100 mb-4">Preview</h2>
+                <PreviewPanel  thumbnail={thumbnail} isLoading={loading} aspectRatio={aspectRatio}/>
+              </div>
             </div>
           </div>
         </main>
